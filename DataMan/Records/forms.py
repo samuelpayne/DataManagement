c345457f2c3ed595e3a4ed9dac7d7ed7720eda70
Zach Olivier <zachary@schoeny.com>
2019-01-18 15:55:06 -0700

    Changed help text on most to verbose text

--- DataMan/Records/forms.py
+++ DataMan/Records/forms.py
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-from django import forms
-from django.core.exceptions import ValidationError
-from Records.models import *
-
-class AddSampleForm(forms.ModelForm):
-    class Meta:
-        model = Sample
-        fields = ['sampleName', 'sampleID', 'experiment',
-                  'storageCondition', 'storageLocation', 'treatmentProtocol',
-                  'dateCreated', 'organism', 'organismModifications']
-=======
 from django import forms
 from django.core.exceptions import ValidationError
 from Records.models import *
@@ -34,4 +22,4 @@ class AddExperimentForm(forms.ModelForm):
         model = Experiment
         fields = ['experimentName','experimentID','projectLead','teamMembers',
                   'IRB','experimentalDesign',]
->>>>>>> 2fd595bee8dfe6b71d19d9c5a7f1981fd61e9b50
+
--- DataMan/Records/models.py
+++ DataMan/Records/models.py
@@ -1,86 +1,24 @@
-<<<<<<< HEAD
 from django.db import models
 
-class Dataset (models.Model):
-    datasetName = models.TextField(help_text='Dataset Name')
-    datasetID = models.IntegerField(help_text='Dataset ID')
+class Dataset(models.Model):
+    datasetName = models.TextField(verbose_name='Dataset Name',
+                                   unique=True)
+    datasetID = models.IntegerField(verbose_name='Dataset ID', primary_key=True,
+                                    unique=True)
     sample = models.OneToOneField('Sample', on_delete=models.CASCADE,
-                                 blank = True, null = True)
+                                  blank=True, null=True)
     # instrument = models.ForeignKey(Instrument, on_delete=models.CASCADE)
-    instrumentSetting = models.TextField(help_text='link to instrument settings')
-    type = models.TextField(help_text='Type of data generated')
-    operator = models.TextField(help_text='Operator')
-    status = models.TextField(help_text='Status')
+    instrumentSetting = models.TextField(verbose_name='link to instrument settings')
+    type = models.TextField(verbose_name='Type of data generated')
+    operator = models.TextField(verbose_name='Operator')
+    status = models.TextField(verbose_name='Status')
     dateCreated = models.DateTimeField()
-    fileLocation = models.TextField(help_text='Path to file location')
-    fileName = models.TextField(help_text='File Name')
-    acquisitionStart = models.TextField(help_text='Acquisition Start')
-    acquisitionEnd = models.TextField(help_text="Acquisition End")
-    fileSize = models.IntegerField(help_text='File Size')
-    fileHash = models.TextField(help_text='File Hash')
-
-    def get_absolute_url(self):
-        return reverse('model-detail-view', args=[str(self.id)])
-
-    def __str__(self):
-        return self.datasetName
-
-class Sample (models.Model):
-    sampleName = models.TextField(help_text='Sample Name')
-    sampleID = models.IntegerField(help_text='Integer Field')
-    experiment = models.ForeignKey('Experiment', on_delete=models.CASCADE,
-                                   blank = True, null = True)
-    #datasets = models.OneToOneField(Dataset, on_delete=models.CASCADE,
-    #                              blank = True, null = True)
-    #preceedingSample = models.TextField(help_text='Preceeding Sample')
-    storageCondition = models.TextField(help_text='Storage Condition')
-    storageLocation = models.TextField(help_text='Path to storage location')
-    treatmentProtocol = models.TextField(help_text='Treatment Protocol')
-    dateCreated = models.DateTimeField()
-    organism = models.TextField(help_text='Organism')
-    organismModifications = models.TextField(help_text='Organism Modifications', default = 'None')
-
-    def get_absolute_url(self):
-        return reverse('sample-detail', args=[str(self.id)])
-
-    def __str__(self):
-        return self.sampleName
-
-class Experiment(models.Model):
-    experimentName = models.TextField(help_text='Experiment Name')
-    experimentID = models.IntegerField(help_text='Experiment ID')
-    projectLead = models.TextField(help_text='Project Lead')
-    teamMembers = models.TextField(help_text='Team Members')
-    IRB = models.IntegerField(help_text='IRB Number')
-    # samples = models.ForeignKey(Sample, on_delete=models.CASCADE)
-    # datasets = models.ForeignKey(Dataset, on_delete=models.CASCADE)
-    experimentalDesign = models.TextField(help_text='Experimental Design')
-
-    def get_absolute_url(self):
-        return reverse('model-detail-view', args=[str(self.id)])
-
-    def __str__(self):
-        return self.experimentName
-=======
-from django.db import models
-
-class Dataset (models.Model):
-    datasetName = models.TextField(help_text='Dataset Name')
-    datasetID = models.IntegerField(help_text='Dataset ID',primary_key = True)
-    sample = models.OneToOneField('Sample',on_delete=models.CASCADE,
-                              blank = False, null = False)
-    # instrument = models.ForeignKey(Instrument, on_delete=models.CASCADE)
-    instrumentSetting = models.TextField(help_text='link to instrument settings')
-    type = models.TextField(help_text='Type of data generated')
-    operator = models.TextField(help_text='Operator')
-    status = models.TextField(help_text='Status')
-    dateCreated = models.DateTimeField()
-    fileLocation = models.TextField(help_text='Path to file location')
-    fileName = models.TextField(help_text='File Name')
-    acquisitionStart = models.TextField(help_text='Acquisition Start')
-    acquisitionEnd = models.TextField(help_text="Acquisition End")
-    fileSize = models.IntegerField(help_text='File Size')
-    fileHash = models.TextField(help_text='File Hash')
+    fileLocation = models.TextField(verbose_name='Path to file location')
+    fileName = models.TextField(verbose_name='File Name')
+    acquisitionStart = models.TextField(verbose_name='Acquisition Start')
+    acquisitionEnd = models.TextField(verbose_name="Acquisition End")
+    fileSize = models.IntegerField(verbose_name='File Size')
+    fileHash = models.TextField(verbose_name='File Hash')
 
     def get_absolute_url(self):
         return reverse('dataset-detail', args=[str(self.id)])
@@ -88,44 +26,49 @@ class Dataset (models.Model):
     def __str__(self):
         return self.datasetName
 
-class Sample (models.Model):
-    sampleName = models.TextField(verbose_name = "Sample Name")
-    sampleID = models.IntegerField(help_text='Integer Field', primary_key = True)
-    experiment = models.ForeignKey('Experiment', on_delete=models.CASCADE,
-                                   blank = False, null = False)
-    #dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE,
-    #                            blank = True, null = True)
-    # preceedingSample = models.TextField(help_text='Preceeding Sample')
-    storageCondition = models.TextField(help_text='Storage Condition')
-    storageLocation = models.TextField(help_text='Path to storage location')
-    treatmentProtocol = models.TextField(help_text='Treatment Protocol')
-    dateCreated = models.DateTimeField()
-    organism = models.TextField(help_text='Organism')
-    organismModifications = models.TextField(help_text='Organism Modifications', default = 'None')
 
-    #this sets the default sort
+class Sample(models.Model):
+    sampleName = models.TextField(verbose_name="Sample Name",
+                                  unique=True)
+    sampleID = models.IntegerField(primary_key=True,
+                                   unique=True)
+    experiment = models.ForeignKey('Experiment', on_delete=models.CASCADE,
+                                   blank=True, null=True)
+    # dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE,
+    #                            blank = True, null = True)
+    # preceedingSample = models.TextField(verbose_name='Preceeding Sample')
+    storageCondition = models.TextField(verbose_name='Storage Condition')
+    storageLocation = models.TextField(verbose_name='Storage Location')
+    treatmentProtocol = models.TextField(verbose_name='Treatment Protocol')
+    dateCreated = models.DateTimeField(help_text='Date the sample was taken')
+    organism = models.TextField(verbose_name='Organism')
+    organismModifications = models.TextField(verbose_name='Organism Modifications', default='None')
+
+    # this sets the default sort
     class Meta:
         ordering = ['sampleName']
-        
+
     def get_absolute_url(self):
         return reverse('sample-detail', args=[self.sampleID])
 
     def __str__(self):
         return self.sampleName
 
+
 class Experiment(models.Model):
-    experimentName = models.TextField(help_text='Experiment Name')
-    experimentID = models.IntegerField(help_text='Experiment ID', primary_key = True)
-    projectLead = models.TextField(help_text='Project Lead')
-    teamMembers = models.TextField(help_text='Team Members')
-    IRB = models.IntegerField(help_text='IRB Number')
-    #samples = models.ForeignKey(Sample, on_delete=models.CASCADE)
-    #datasets = models.ForeignKey(Dataset, on_delete=models.CASCADE)
-    experimentalDesign = models.TextField(help_text='Experimental Design')
+    experimentName = models.TextField(verbose_name='Experiment Name',
+                                      unique=True)
+    experimentID = models.IntegerField(verbose_name='Experiment ID', primary_key=True,
+                                       unique=True)
+    projectLead = models.TextField(verbose_name='Project Lead')
+    teamMembers = models.TextField(verbose_name='Team Members')
+    IRB = models.IntegerField(verbose_name='IRB Number')
+    # samples = models.ForeignKey(Sample, on_delete=models.CASCADE)
+    # datasets = models.ForeignKey(Dataset, on_delete=models.CASCADE)
+    experimentalDesign = models.TextField(verbose_name='Experimental Design')
 
     def get_absolute_url(self):
         return reverse('experiment-detail', args=[str(self.id)])
 
     def __str__(self):
         return self.experimentName
->>>>>>> 2fd595bee8dfe6b71d19d9c5a7f1981fd61e9b50
